8261ac86fb493e5e32e06f820610d5352cca8dc8
Diego Zamora <diegozar02@gmail.com>
2026-06-05 11:03:11 -0600

    Refactor configuration files and improve index page layout

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,10 @@ import tseslint from "typescript-eslint";
 import astro from "eslint-plugin-astro";
 
 export default tseslint.config(
-  {
-    ignores: ["dist/", ".astro/", "node_modules/"],
-  },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...astro.configs.recommended,
+    {
+        ignores: ["dist/", ".astro/", "node_modules/"],
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...astro.configs.recommended,
 );
--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import("prettier").Config} */
 export default {
-  printWidth: 100,
-  singleQuote: false,
-  semi: true,
-  plugins: ["prettier-plugin-astro"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: { parser: "astro" },
-    },
-  ],
+    printWidth: 100,
+    singleQuote: false,
+    semi: true,
+    tabWidth: 4,
+    plugins: ["prettier-plugin-astro"],
+    overrides: [
+        {
+            files: "*.astro",
+            options: { parser: "astro" },
+        },
+    ],
 };
